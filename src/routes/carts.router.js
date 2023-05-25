f31eae2250d261fb06e271cc4e46f1721cd01d47
Claudio Cabanas <ccabanas@alonenet.com.ar>
2023-05-24 21:52:38 -0300

    prueba5

--- src/routes/carts.router.js
+++ src/routes/carts.router.js
@@ -54,7 +54,6 @@ router.post("/:cid/product/:pid", async (req, res) => {
   } catch (error) {
     console.log(error);
   }
-
   // try {
   //   const cart = await cartManager.updateProductCartById(
   //     Number(cid),
